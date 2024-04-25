1a0815d17622ce04e41cc05c43d3db9198a7048d
saran-99629 <s.saran99629@gmail.com>
2024-04-25 14:37:56 +0530

    feature - email feature backend complete

--- authentication/views.py
+++ authentication/views.py
@@ -3,9 +3,14 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-
-
-
+from login import settings
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from . tokens import generate_token
+from django.core.mail import EmailMessage, send_mail
 # Create your views here.
 def home(request):
     return render(request,"authentication/index.html")
@@ -37,14 +42,51 @@ def signup(request):
         if pass1 != pass2 :
             messages.error(request, "Password didn't Matched !")
         if not username.isalnum():
-            messages.error(request, "Username is AlphaNumerical!!")
+            messages.error(request, "Username must be AlphaNumerical!!")
+            return redirect('home')
+        
+        
         try:
             myuser = User.objects.create_user(username, email, pass1)
             myuser.first_name = fname
             myuser.last_name = lname
             myuser.save()
-            messages.success(request, "Your account has been successfully created.")
+            myuser.is_active = False 
+            messages.success(request, "Your account has been successfully created. We have send you a confirmation email, please confirm your email  in order to activate the account")
+            
+            # Welcom Email
+            subject = "Welcome to Spike - Django Login !!"
+            message = "Hello" + myuser.first_name + "!! \n" + "Welcome to Spike !! \n Thank you for visting my website \n We have also sent you a Confirmation email, Please conform your email address in order to activate your account. \n\n Thanking you \n saran"
+            from_email = settings.EMAIL_HOST_USER
+            to_list = [myuser.email]
+            send_mail(subject, message, from_email, to_list, fail_silently=True)
+            
+            #confirmation email 
+
+            current_site = get_current_site(request)
+            email_subject = "Confirm Your email @ Spike - Django Login !!"
+            message2 = render_to_string('email_confirmation.html', { # type: ignore
+                'name' : myuser.first_name,
+                'domain' : current_site.domain,
+                'uid' : urlsafe_base64_encode(force_bytes(myuser.pk)),
+                'token': generate_token.make_token(myuser),
+            })
+
+            email = EmailMessage(
+                email_subject,
+                message2, 
+                settings.EMAIL_HOST_USER,
+                [myuser.email],
+            )
+
+            email.fail_silently = True 
+            email.send()
+
             return redirect('/signin')
+            
+
+            
+
         except IntegrityError:
             messages.error(request, "Username already exists. Please choose a different username.")
             return redirect('/signup')
@@ -70,3 +112,17 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged out Successfully !")
     return redirect('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    else:
+        return render(request, 'activation_failed.html')
