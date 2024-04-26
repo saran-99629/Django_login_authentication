9738be0e9f8dc4007b7e5251ed4c624846b063e0
saran-99629 <s.saran99629@gmail.com>
2024-04-26 11:20:32 +0530

    feature - debugged the email confirmation mail and login successful mail

--- login/info.py
+++ login/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'login.demo.django.login@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'spikeweb143@gmail.com'
+EMAIL_HOST_PASSWORD = 'rvpr wczd ykiz wmbx'
 EMAIL_PORT = 587
